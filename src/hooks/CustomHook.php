590d2ee817eef5dc2a4228500789bc79522e5143
elmasrisaer <139119606+elmasrisaer@users.noreply.github.com>
2024-05-29 15:12:50 +0300

    liblab SDK update for version v1.1.65 (#14)

--- src/hooks/CustomHook.php
+++ src/hooks/CustomHook.php
@@ -15,10 +15,17 @@ class CustomHook implements HookInterface
     private $token;
     private $tokenExpiration;
 
+    /**
+     * @param array{
+     *   client_id: string,
+     *   client_secret: string
+     * } $params
+     */
+
     public function __construct($clientId, $clientSecret)
     {
-        $this->clientId = $clientId;
-        $this->clientSecret = $clientSecret;
+        $this->clientId = $params['client_id'];
+        $this->clientSecret = $params['client_secret'];
         $this->token = null;
         $this->tokenExpiration = null;
     }
